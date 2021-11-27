9fc332e324d147cb381e48a0e53658ea6c0875b1
RayendraSabandar <rayendratimotius@gmail.com>
2021-11-27 11:45:36 +0700

    Created controller for users

---
+++ server/src/users/users.controller.ts
@@ -0,0 +1,46 @@
+import { Controller, Get, Post, Put, Delete, Body, Param, Patch } from "@nestjs/common";
+import { UsersService } from "./users.service";
+
+
+@Controller('users')
+export class UsersController {
+    constructor(private readonly userService: UsersService) {}
+
+    @Post()
+    registerUser(
+        @Body('name') name: string,
+        @Body('email') email: string,
+        @Body('password') password: string,
+        @Body('avatar') avatar: string,
+    ) {
+        const generatedId = this.userService.registerUser(name, email, password, avatar)
+        return {
+            id: generatedId
+        };
+    }
+
+    @Get()
+    getAllUsers(){
+        return this.userService.getAllUsers()
+    }
+
+    @Get(':id')
+    getUserById(@Param('id') id: string){
+        return this.userService.getUserById(id)
+    }
+
+    @Patch(':id')
+    editAvatar(
+        @Param('id') id: string,
+        @Body('avatar') avatar: string
+    ){
+        return this.userService.editAvatar(id, avatar)
+    }
+
+    @Delete(':id')
+    deleteUser(
+        @Param('id') id: string,
+    ){
+        return this.userService.deleteUser(id)
+    }
+}
